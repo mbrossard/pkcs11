67d1ce3b106989dacfa1d6863ab473ecdf1f6b6e
Mathias Brossard <mathias@brossard.org>
2015-10-08 23:48:38 -0700

    Off-by-one error

--- src/pkcs11-util.c
+++ src/pkcs11-util.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 {
     int r = 0;
 
-    if(argc <= 0) {
+    if(argc <= 1) {
         usage();
     } else if(!strcmp(argv[1], "help") ||
               !strcmp(argv[1], "--help") ||

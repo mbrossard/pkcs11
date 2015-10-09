f4f85c6ba9b0dc85664c845f8fd8ab867c6a2ef8
Mathias Brossard <mathias@brossard.org>
2015-10-09 00:08:01 -0700

    Remove key generation from list command

--- src/list.c
+++ src/list.c
@@ -10,7 +10,6 @@
 
 #include "crypto.h"
 #include "common.h"
-#include "keypair.h"
 #include "pkcs11_display.h"
 
 int do_list_token_objects(CK_FUNCTION_LIST *funcs,
@@ -208,8 +207,6 @@ static const struct option options[] = {
     { "slot",               1, 0,           's' },
     { "module",             1, 0,           'm' },
     { "directory",          1, 0,           'd' },
-    { "genkey",             1, 0,           'g' },
-    { "label",              1, 0,           'l' },
     { 0, 0, 0, 0 }
 };
 
@@ -223,8 +220,6 @@ static const char *option_help[] = {
     "Specify number of the slot to use",
     "Specify the module to load",
     "Specify the directory for NSS database",
-    "Generate key",
-    "Set label on generated keys",
 };
 
 
@@ -243,7 +238,6 @@ int list( int argc, char **argv )
     CK_RV             rc;
     CK_ULONG          opt_slot = -1;
     char *opt_module = NULL, *opt_dir = NULL;
-    char *gen_param = NULL;
     int long_optind = 0;
     int do_show_info = 0;
     int do_list_slots = 0;
@@ -251,14 +245,13 @@ int list( int argc, char **argv )
     int do_list_objects = 0;
     int need_session = 0;
     int action_count = 0;
-    int genkey = 0;
 
     char c;
 
     init_crypto();
 
     while (1) {
-        c = getopt_long(argc, argv, "ILMOd:hl:p:s:g:m:",
+        c = getopt_long(argc, argv, "ILMOd:hp:s:m:",
                         options, &long_optind);
         if (c == -1)
             break;
@@ -297,11 +290,6 @@ int list( int argc, char **argv )
                 break;
             case 'm':
                 opt_module = optarg;
-                break;
-            case 'g':
-                need_session |= NEED_SESSION_RW;
-                gen_param = optarg;
-                genkey = 1;
                 break;
             case 'h':
             default:
@@ -364,78 +352,6 @@ int list( int argc, char **argv )
 
         if(do_list_objects) {
             do_list_token_objects(funcs, pslots[islot], opt_pin, opt_pin_len);
-        }
-    }
-
-    if(genkey) {
-
-        if(opt_slot == -1) {
-            rc = funcs->C_GetSlotList(0, NULL_PTR, &nslots);
-            if (rc != CKR_OK) {
-                show_error(stdout, "C_GetSlotList", rc );
-                return rc;
-            }
-
-            if(nslots == 1) {
-                rc = funcs->C_GetSlotList(0, &opt_slot, &nslots);
-                if (rc != CKR_OK) {
-                    show_error(stdout, "C_GetSlotList", rc );
-                    return rc;
-                } else {
-                    printf("Using slot %ld\n", opt_slot);
-                }
-            }
-        }
-
-        if(opt_slot != -1) {
-            char             *tmp;
-            long              keysize;
-            CK_SESSION_HANDLE h_session;
-            CK_FLAGS          flags = CKF_SERIAL_SESSION | CKF_RW_SESSION;
-            rc = funcs->C_OpenSession( opt_slot, flags, NULL, NULL, &h_session );
-            if(opt_pin_len) {
-                rc = funcs->C_Login( h_session, CKU_USER, opt_pin, opt_pin_len );
-                if (rc != CKR_OK) {
-                    show_error(stdout, "C_Login", rc );
-                }
-            } else {
-                CK_TOKEN_INFO  info;
-
-                rc = funcs->C_GetTokenInfo( opt_slot, &info );
-                if (rc != CKR_OK) {
-                    show_error(stdout, "C_GetTokenInfo", rc );
-                    return FALSE;
-                }
-
-                if(info.flags & CKF_PROTECTED_AUTHENTICATION_PATH) {
-                    rc = funcs->C_Login( h_session, CKU_USER, NULL, 0 );
-                    if (rc != CKR_OK) {
-                        show_error(stdout, "C_Login", rc );
-                        return rc;
-                    }
-                }
-            }
-            fprintf(stdout, "Generating key with param '%s'\n", gen_param);
-            keysize = strtol(gen_param, &tmp, 10);
-            if(gen_param != tmp) {
-                fprintf(stdout, "Generating RSA key with size %ld\n", keysize);
-                rc = generateRsaKeyPair(funcs, h_session, keysize, opt_label);
-            } else if(strncmp(gen_param, "gost", 4) == 0) {
-                fprintf(stdout, "Generating GOST R34.10-2001 key (%s) in slot %ld\n",
-                        gen_param, opt_slot);
-                rc = generateGostKeyPair(funcs, h_session, gen_param, opt_label);
-            } else {
-                CK_BBOOL full;
-                rc = ecdsaNeedsEcParams(funcs, opt_slot, &full);
-                if(rc == CKR_OK) {
-                    fprintf(stdout, "Generating ECDSA key with curve '%s' "
-                            "in slot %ld with %s\n", gen_param, opt_slot,
-                            full ? "EC Parameters" : "Named Curve");
-                    rc = generateEcdsaKeyPair(funcs, h_session, gen_param, full, opt_label);
-                }
-            }
-        } else {
-            printf("The key generation function requires the '--slot' parameter\n");
         }
     }
 

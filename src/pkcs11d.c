4c61a083d11f3cd7df6f68bf4d007dedda6d240d
Mathias Brossard <mathias@brossard.org>
2016-02-25 22:55:24 -0800

    Add command line options

--- src/pkcs11d.c
+++ src/pkcs11d.c
@@ -2,7 +2,25 @@
  * Copyright (C) 2016 Mathias Brossard <mathias@brossard.org>
  */
 
+#include "common.h"
 #include "network.h"
+static char *app_name = "pkcs11d";
+
+static const struct option options[] = {
+    { "help",               0, 0,           'h' },
+    { "pin",                1, 0,           'p' },
+    { "slot",               1, 0,           's' },
+    { "module",             1, 0,           'm' },
+    { 0, 0, 0, 0 }
+};
+
+static const char *option_help[] = {
+    "Print this help and exit",
+    "Supply PIN on the command line",
+    "Specify number of the slot to use",
+    "Specify the module to load",
+};
+
 int main(int argc, char **argv)
 {
     struct sockaddr_un sockaddr;

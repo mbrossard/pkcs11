d823a4cc01da39faeedb889d05a6308d786cb0dc
Mathias Brossard <mathias@brossard.org>
2016-03-08 02:23:55 -0800

    Call function to list RSA keys

--- src/pkcs11d.c
+++ src/pkcs11d.c
@@ -147,6 +147,8 @@ int main(int argc, char **argv)
         return rc;
     }
     
+    list_rsa_objects(funcs, h_session)
+
     fd = nw_unix_server("pkcs11d.sock", &sockaddr, 0, 0, 0, 64);
     close(fd);
     fd = nw_tcp_server(1234, 0, 64);

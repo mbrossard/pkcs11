b213a0b02de4087879467f6c8b6d937864ee3722
Mathias Brossard <mathias@brossard.org>
2016-04-19 00:35:30 -0700

    Remove superfluous files

--- src/ec-utils.c
+++
@@ -1,7 +0,0 @@
-#include "ec-utils.h"
-
-ASN1_SEQUENCE(ECDSA_signature_request) = {
-        ASN1_SIMPLE(ECDSA_signature_request, digest, ASN1_OCTET_STRING),
-        ASN1_SIMPLE(ECDSA_signature_request, inv, BIGNUM),
-        ASN1_SIMPLE(ECDSA_signature_request, rp, BIGNUM)
-} ASN1_SEQUENCE_END(ECDSA_signature_request)

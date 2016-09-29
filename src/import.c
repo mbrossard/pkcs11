acc4f53789384ea1009cea3b824668e028d137a2
Mathias Brossard <mathias@brossard.org>
2016-09-29 00:19:23 -0700

    Add usual command-line parameters

--- src/import.c
+++ src/import.c
@@ -12,25 +12,52 @@ static const char *app_name = "pkcs11-util import";
 
 static const struct option options[] = {
     { "help",               0, 0,           'h' },
+    { "pin",                1, 0,           'p' },
+    { "slot",               1, 0,           's' },
+    { "module",             1, 0,           'm' },
+    { "directory",          1, 0,           'd' },
     { 0, 0, 0, 0 }
 };
 
 static const char *option_help[] = {
     "Print this help and exit",
+    "Supply PIN on the command line",
+    "Specify number of the slot to use",
+    "Specify the module to load",
+    "Specify the directory for NSS database",
 };
 
 int import(int argc, char **argv)
 {
+    CK_UTF8CHAR_PTR   opt_pin = NULL;
+    CK_ULONG          opt_pin_len = 0;
+    CK_ULONG          opt_slot = -1;
     CK_RV             rc = 0;
+    char *opt_module = NULL, *opt_dir = NULL;
     int long_optind = 0;
     char c;
 
     while (1) {
-        c = getopt_long(argc, argv, "h",
+        c = getopt_long(argc, argv, "d:h:p:s:m:",
                         options, &long_optind);
         if (c == -1)
             break;
         switch (c) {
+            case 'd':
+                opt_dir = optarg;
+                break;
+            case 'p':
+                opt_pin = (CK_UTF8CHAR_PTR) strdup(optarg);
+                if(opt_pin) {
+                    opt_pin_len = strlen(optarg);
+                }
+                break;
+            case 's':
+                opt_slot = (CK_SLOT_ID) atoi(optarg);
+                break;
+            case 'm':
+                opt_module = optarg;
+                break;
             case 'h':
             default:
                 print_usage_and_die(app_name, options, option_help);

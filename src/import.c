8380750dc1aea2b534f105986cc75d954c3350e9
Mathias Brossard <mathias@brossard.org>
2016-09-28 21:58:54 -0700

    Start on import feature

---
+++ src/import.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2016 Mathias Brossard <mathias@brossard.org>
+ */
+
+#include "config.h"
+#include "common.h"
+
+static const char *app_name = "pkcs11-util import";
+
+static const struct option options[] = {
+};
+
+static const char *option_help[] = {
+};
+
+int import(int argc, char **argv)
+{
+}

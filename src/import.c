898ada8f9c9fb9cd5a8466d873227af7fe824718
Mathias Brossard <mathias@brossard.org>
2016-09-28 22:05:06 -0700

    Start adding some content to import function

--- src/import.c
+++ src/import.c
@@ -11,13 +11,35 @@
 static const char *app_name = "pkcs11-util import";
 
 static const struct option options[] = {
+    { "help",               0, 0,           'h' },
+    { 0, 0, 0, 0 }
 };
 
 static const char *option_help[] = {
+    "Print this help and exit",
 };
 
 int import(int argc, char **argv)
 {
+    CK_RV             rc = 0;
+    int long_optind = 0;
+    char c;
+
+    while (1) {
+        c = getopt_long(argc, argv, "h",
+                        options, &long_optind);
+        if (c == -1)
+            break;
+        switch (c) {
+            case 'h':
+            default:
+                print_usage_and_die(app_name, options, option_help);
+        }
+    }
+
+    print_usage_and_die(app_name, options, option_help);
+
+    return rc;
 }
 
 #endif

4f454365c9cc4c080378774201cf2f1fada19f74
Mathias Brossard <mathias@brossard.org>
2016-10-03 23:17:32 -0700

    Check slot is present

--- src/import.c
+++ src/import.c
@@ -120,12 +120,17 @@ int import(int argc, char **argv)
         return rc;
     }
 
-    if(opt_slot != -1) {
-        /* TODO: Look in pslots */
-        pslots = &opt_slot;
-        nslots = 1;
+    if (opt_slot != -1) {
+        CK_ULONG i = 0;
+        while (i < nslots && pslots[i] != opt_slot) {
+            i++;
+        }
+        if (i == nslots) {
+            fprintf(stderr, "Unknown slot '%lu'\n", opt_slot);
+            return -1;            
+        }
     } else {
-        if(nslots == 1) {
+        if (nslots == 1) {
             opt_slot = pslots[0];
         } else {
             fprintf(stdout, "Found %ld slots, use --slot parameter to choose.\n", nslots);

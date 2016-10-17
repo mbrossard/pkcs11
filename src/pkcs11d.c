f66c5a68dd8502af3113e6fd0e8b91535871eb96
Mathias Brossard <mathias@brossard.org>
2016-10-16 23:43:52 -0700

    Fix if whitespace

--- src/pkcs11d.c
+++ src/pkcs11d.c
@@ -186,7 +186,7 @@ int main(int argc, char **argv)
         return rc;
     }
 
-    if(opt_slot != -1) {
+    if (opt_slot != -1) {
         CK_ULONG i = 0;
         while (i < nslots && pslots[i] != opt_slot) {
             i++;
@@ -196,7 +196,7 @@ int main(int argc, char **argv)
             return -1;            
         }
     } else {
-        if(nslots == 1) {
+        if (nslots == 1) {
             opt_slot = pslots[0];
         } else {
             fprintf(stdout, "Found %ld slots, use --slot parameter to choose.\n", nslots);

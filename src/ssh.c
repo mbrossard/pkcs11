4d57dc40ed19ce05c465b4e10a5095d0f78ff9ee
Mathias Brossard <mathias@brossard.org>
2016-10-21 00:12:56 -0700

    Remove warning for unused variable

--- src/ssh.c
+++ src/ssh.c
@@ -296,7 +296,7 @@ static const char *option_help[] = {
 
 int ssh(int argc, char **argv)
 {
-    CK_ULONG          nslots, islot;
+    CK_ULONG          nslots;
     CK_SLOT_ID        *pslots = NULL;
     CK_FUNCTION_LIST  *funcs = NULL;
     CK_UTF8CHAR_PTR   opt_pin = NULL;

97789f14d1665840637e318b6a07d32fe357a83d
Mathias Brossard <mathias@brossard.org>
2016-10-27 02:38:04 -0700

    Add declaration for generateSessionKey

--- src/key.h
+++ src/key.h
@@ -15,6 +15,10 @@ CK_RV generateKey(CK_FUNCTION_LIST_PTR p11, CK_SESSION_HANDLE session,
                   CK_KEY_TYPE type, CK_ULONG mech, CK_ULONG size,
                   CK_BYTE_PTR label);
 
+CK_RV generateSessionKey(CK_FUNCTION_LIST_PTR p11, CK_SESSION_HANDLE session,
+                         CK_KEY_TYPE type, CK_ULONG mech, CK_ULONG size,
+                         CK_OBJECT_HANDLE_PTR phKey);
+
 #ifdef __cplusplus
 };
 #endif
